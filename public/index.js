8f7e32b104a07b82d2e76ed5617793cb0bafb2d6
Adrien Vergnaud <a.vergnaud@gmail.com>
2019-02-23 16:24:12 +0100

    securite

--- public/index.js
+++ public/index.js
@@ -1,4 +1,10 @@
-function loadDoc() {
+var frontModule = (function() {
+
+  /* id_token oAuth Google */
+  var id_token = "";
+
+  /* au clic sur Envoyer */
+  function envoyerJoueur() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
@@ -11,6 +17,41 @@ function loadDoc() {
       "application/x-www-form-urlencoded"
     );
     xhttp.send(
-      "username=" + document.getElementById("userNameInput").value
+      "username=" + document.getElementById("userNameInput").value + "&id_token=" + id_token
     );
   }
+
+  /* gestion sécurité authent Google */
+  function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile()
+    id_token = googleUser.getAuthResponse().id_token
+    var xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("signed-in-mail").innerHTML = "Signed in as: " + profile.getEmail()
+      }
+    }
+    xhttp.open("POST", "/tokensignin", true);
+    xhttp.setRequestHeader(
+      "Content-type",
+      "application/x-www-form-urlencoded"
+    )
+    xhttp.send("idtoken=" + id_token)
+  }
+
+  /* gestion sécurité authent Google */
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function() {
+      console.log("User signed out.");
+    });
+  }
+
+  return {
+    envoyerJoueur: envoyerJoueur,
+    onSignIn: onSignIn,
+    signOut: signOut
+  }
+})();
+
+window.onSignIn = frontModule.onSignIn
--- server.js
+++ server.js
@@ -1,15 +1,37 @@
-const express = require("express");
-const path = require("path");
-const app = express();
+const express = require("express")
+const path = require("path")
+const {OAuth2Client} = require('google-auth-library')
+require('dotenv').config()
+
+authorized_google_id = process.env.authorized_google_id
+
+const app = express()
+const googleClientId = "955369598263-41nek13o5fk17ituf5dn7psib3sesmpn.apps.googleusercontent.com"
+const client = new OAuth2Client(googleClientId)
 
 app.use(express.urlencoded());
 app.use(express.static('public'))
 
+app.post("/tokensignin", (req, res) => {
+  verify(req.body.idtoken)
+  .then(success => {
+    res.send("ok");
+  })
+  .catch(error => {
+    res.status(403).send("erreur")
+  });
+});
+
 app.post("/submit-form", (req, res) => {
   const username = req.body.username;
-  //...
-  console.log(username);
-  res.send({ hello: 'world' });
+  const id_token = req.body.id_token;
+  verify(id_token)
+  .then(success => {
+    res.send("transaction en cours pour " + username + " ...");
+  })
+  .catch(error => {
+    res.send("erreur")
+  });
 });
 
 app.get("/", (req, res) => {
@@ -19,6 +41,17 @@ app.get("/", (req, res) => {
 const server = app.listen(8080, () => {
   const host = server.address().address;
   const port = server.address().port;
-
   console.log(`Example app listening at http://${host}:${port}`);
 });
+
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: googleClientId
+  });
+  const payload = ticket.getPayload();
+  var userid = payload['sub'];
+  if(userid !== authorized_google_id) {
+    throw new Error("userid non autorisé");
+  }
+}
